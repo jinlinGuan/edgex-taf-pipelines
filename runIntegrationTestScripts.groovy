13f6cb80cfe3cc11e1ea248751a8c1e2ca0dd017
Ginny Guan <ginny@iotechsys.com>
2022-02-23 17:42:43 +0800

    change taf repo
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- runIntegrationTestScripts.groovy
+++ runIntegrationTestScripts.groovy
@@ -14,7 +14,7 @@ def main() {
                     doGenerateSubmoduleConfigurations: false,
                     extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '']],
                     submoduleCfg: [],
-                    userRemoteConfigs: [[url: 'https://github.com/edgexfoundry/edgex-taf.git']]
+                    userRemoteConfigs: [[url: 'https://github.com/jinlinGuan/edgex-taf.git']]
                 ])
             }
             stage ("Retrieve Compose File - ${ARCH}${USE_SECURITY}${TAF_BRANCH}") {
@@ -68,7 +68,7 @@ def main() {
                     sh "cp ../edgex/report.xml redis-bus-shutdown-report.xml"
                 }
             }
-
+/*
             stage ("Deploy EdgeX - MQTT Bus - ${ARCH}${USE_SECURITY}${TAF_BRANCH}") {
                 def deployMQTTLog= sh (
                     script: "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:z -w ${env.WORKSPACE} \
@@ -115,7 +115,7 @@ def main() {
                     sh "cp ../edgex/report.xml mqtt-bus-shutdown-report.xml"
                 }
             }
-
+*/
             stage ("Stash Report - ${ARCH}${USE_SECURITY}${TAF_BRANCH}") {
                 echo '===== Merge Reports ====='
                 sh "docker run --rm --network host -v ${env.WORKSPACE}:${env.WORKSPACE}:rw,z -w ${env.WORKSPACE} \
